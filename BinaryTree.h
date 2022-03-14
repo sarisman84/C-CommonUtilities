4ded69235efec5f56baf73add6c93e845d82da18
Spyro (Home) <epicdudeness677@gmail.com>
2022-03-14 01:17:32 +0100

    Did a thing

--- BinaryTree.h
+++ BinaryTree.h
@@ -1,8 +1,24 @@
 #pragma once
+#include <Windows.h>
+#include <iostream>
+#include <sstream>
+
+#include <functional>
+
+#define DBOUT( s )            \
+{                             \
+   std::ostringstream os_;    \
+   os_ << s;                   \
+   OutputDebugStringA(os_.str().c_str() );  \
+}
 
 
 namespace CommonUtilities
 {
+
+
+
+
 	template <class T>
 	struct Node
 	{
@@ -18,13 +34,26 @@ namespace CommonUtilities
 
 
 
-
-
-		Node* GetNextNode(const T& aValue)
-		{
-			return myData < aValue ? myLeftNode : myRightNode;
-		}
 	};
+
+
+	template<class T>
+	struct SearchResults
+	{
+		Node<T>* myTargetNode;
+		Node<T>* myParentNode;
+	};
+
+
+	template <class T>
+	struct ComparisonResults
+	{
+		SearchResults<T> mySearchResults;
+		int myChildSide;
+	};
+
+
+
 
 
 
@@ -47,118 +76,128 @@ namespace CommonUtilities
 		{
 			return Contains(myHeadNode, aValue);
 		}
-		//Inserts the element into the tree if it doesn't already exist otherwise does nothing.
+		//Inserts the element into the tree if it doesn't already exist, otherwise does nothing.
 		void Insert(const T& aValue)
 		{
-			if (!Contains(myHeadNode, aValue))
-			{
-				Node<T>* parentNode;
-				Node<T>* potentialSlot = GetChild(myHeadNode, aValue);
-				while (potentialSlot)
-				{
-
-					Node<T>* slot = GetChild(potentialSlot, aValue);
-					if (!slot)
-					{
-						parentNode = potentialSlot;
-						potentialSlot = slot;
-					}
-				}
-
-				SetChild(parentNode, new Node<T>(aValue), parentNode->myData < aValue ? 1 : 0);
-
-
-			}
-
+			if (Contains(myHeadNode, aValue)) return;
+			Insert(new Node<T>(aValue));
 		};
+
+
+
 		//If the element exist in the tree removes it.
 		void Remove(const T& aValue)
 		{
-			if (Contains(myHeadNode, aValue))
+			if (!Contains(myHeadNode, aValue)) return;
+
+			if (!myHeadNode) return;
+
+			Node<T>* parent = nullptr;
+			Node<T>* node = myHeadNode;
+			bool isLeft = false;
+			while (node->myData != aValue)
 			{
-				Node<T>* parentNode;
-				Node<T>* targetNodeToRemove = GetChild(myHeadNode, aValue);
-				while (targetNodeToRemove->myData != aValue)
-				{
-
-					Node<T>* slot = GetChild(targetNodeToRemove, aValue);
-					if (slot->myData == aValue)
-					{
-						parentNode = targetNodeToRemove;
-						targetNodeToRemove = slot;
-					}
-				}
-
-				Node<T>* leftChild = targetNodeToRemove->myLeftNode;
-				Node<T>* rightChild = targetNodeToRemove->myRightNode;
+				auto result = GetNextNode(node, aValue);
+				node = result.mySearchResults.myTargetNode;
+				parent = result.mySearchResults.myParentNode;
+				isLeft = result.myChildSide == 1;
+			}
 
 
-				delete targetNodeToRemove;
+			if (parent)
+				if (isLeft)
+					parent->myLeftNode = nullptr;
+				else
+					parent->myRightNode = nullptr;
 
+			Node<T>* leftChild = node->myLeftNode;
+			Node<T>* rightChild = node->myRightNode;
+
+			delete node;
+			if (leftChild)
 				Insert(leftChild);
+			if (rightChild)
 				Insert(rightChild);
 
-			}
+
+
+
+
 		};
 
 
 
 	private:
 
-		void Insert(Node<T>*& aNode)
+		void Insert(Node<T>* aNode)
 		{
-			if (!Contains(myHeadNode, aNode->myData))
+			if (!aNode) return;
+			if (!myHeadNode)
 			{
-				Node<T>* parentNode;
-				Node<T>* potentialSlot = GetChild(myHeadNode, aNode->myData);
-				while (potentialSlot)
-				{
+				myHeadNode = aNode;
 
-					Node<T>* slot = GetChild(potentialSlot, aNode->myData);
-					if (!slot)
-					{
-						parentNode = potentialSlot;
-						potentialSlot = slot;
-					}
-				}
+				return;
+			}
 
-				SetChild(parentNode, aNode, (parentNode->myData < aNode->myData) ? 1 : 0);
 
+			DBOUT("Value is not in tree! Inserting!");
+
+
+			Node<T>* parentNode = nullptr;
+			Node<T>* availableNode = myHeadNode;
+			bool isLeft = false;
+
+			while (availableNode)
+			{
+
+
+				ComparisonResults<T> results = GetNextNode(availableNode, aNode->myData);
+
+				availableNode = results.mySearchResults.myTargetNode;
+				parentNode = results.mySearchResults.myParentNode;
+
+				isLeft = results.myChildSide == 1;
+
+				DBOUT(availableNode ? "Still trying to find an empty slot" : "Found empty slot, exiting!");
 
 			}
 
-		};
+			if (parentNode)
+			{
+				if (isLeft)
+					parentNode->myLeftNode = aNode;
+				else
+					parentNode->myRightNode = aNode;
+			}
+		}
 
 
-		bool Contains(Node<T>* aNode, const T& aValue) const
+
+		const bool Contains(Node<T>* aNode, const T& aValue) const
 		{
 			if (!aNode) return false;
-			if (aNode->myData == aValue) return true;
-
-			if (Contains(GetChild(aNode, aValue), aValue))
-			{
-				return true;
-			}
-
-			return false;
+			Node<T>* nextNode = GetNextNode(aNode, aValue).mySearchResults.myTargetNode;
+			if (!nextNode) return false;
+			if (nextNode == aNode) return true;
+			return Contains(nextNode, aValue);
 		}
 
-		Node<T>* GetChild(Node<T>* aParent, const T& aValue) const
+
+
+
+
+
+
+		ComparisonResults<T> GetNextNode(Node<T>* aParentNode, const T& aValue) const
 		{
-			return aParent->myData > aValue ? aParent->myRightNode : aParent->myLeftNode;
+			if (!aParentNode) return ComparisonResults<T>();
+
+			bool isLeft = aParentNode->myData > aValue;
+			bool isRight = aParentNode->myData < aValue;
+			Node<T>* result = isLeft ? aParentNode->myLeftNode : isRight ? aParentNode->myRightNode : aParentNode;
+
+			return ComparisonResults<T>{ SearchResults<T>{result, aParentNode}, isLeft ? 1 : isRight ? 2 : 0 };
 		}
-
-		void SetChild(Node<T>* aParent, Node<T>* aChild, const int aSide = 0)
-		{
-			if (aSide <= 0)
-				aParent->myLeftNode = aChild;
-			else
-				aParent->myRightNode = aChild;
-		}
-
-
-
-
 
 		Node<T>* myHeadNode;
 	};
