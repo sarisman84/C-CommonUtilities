038ccd48c83583d53c268d53372e1242e078d062
Spyridon Passas <epicdudeness677@gmail.com>
2022-04-11 09:33:32 +0200

    Init HashMap.

---
+++ HashMap.h
@@ -0,0 +1,24 @@
+#pragma once
+namespace CommonUtilities
+{
+	template <class Key, class Value>
+	class HashMap
+	{
+	public:
+		HashMap(int aCapacity)
+		{
+		}
+		bool Insert(const Key& aKey, const Value& aValue)
+		{
+		}
+		bool Remove(const Key& aKey)
+		{
+		}
+		const Value* Get(const Key& aKey) const
+		{
+		}
+		Value* Get(const Key& aKey)
+		{
+		}
+	};
+}
